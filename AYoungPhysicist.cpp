048855804942e1d32488668e7b7af38acf063936
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2022-06-30 21:55:58 +0530

    Create AYoungPhysicist.cpp

---
+++ AYoungPhysicist.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int x=0,y=0,z=0;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        x=x+a;
+        y=y+b;
+        z=z+c; }
+        if(x==0 and y==0 and z==0){
+            cout<<"Yes"<<endl;}
+        else{
+            cout<<"No"<<endl;}
+}
