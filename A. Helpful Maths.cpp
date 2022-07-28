e13e5cd48a28d575b73eb07abf190eb5a1cd6f6f
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2022-07-28 12:39:03 +0530

    Create A. Helpful Maths.cpp

---
+++ A. Helpful Maths.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    int n=s.size();
+    for(int i=0;i<n-1;i=i+2){
+    for(int j=0;j<n-i-1;j=j+2){
+      if(s[j]>s[j+2]) swap(s[j],s[j+2]);
+    }
+  }
+    cout<<s<<endl;
+    return 0;
+}
