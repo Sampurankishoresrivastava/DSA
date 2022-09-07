628e03b7e7f82b91a37b56c7549797a3f5fc40e1
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2022-09-07 23:32:02 +0530

    Create Broken Phone_Codechef.cpp

---
+++ Broken Phone_Codechef.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x<y){
+	        cout<<"REPAIR"<<endl;
+	    }
+	    else if(y<x){
+	        cout<<"NEW PHONE"<<endl;
+	    }
+	    else{
+	        cout<<"ANY"<<endl;
+	    }
+	}
+	return 0;
+}
