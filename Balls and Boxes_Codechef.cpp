b4f241c91671f5aed157eaa8501ab574700bbd7d
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2022-09-07 23:35:34 +0530

    Create Balls and Boxes_Codechef.cpp

---
+++ Balls and Boxes_Codechef.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    int sum=(k*(k+1))/2;
+	    if(n>=sum){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
