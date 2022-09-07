001c062b446bf6e3886096564903057d9003f5d0
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2022-09-08 00:39:16 +0530

    Create 1221. Split a String in Balanced Strings.cpp

---
+++ leetcode/1221. Split a String in Balanced Strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int count=0,r=0,l=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='R'){
+                r++;
+            }       
+            else if(s[i]=='L'){
+                l++;
+            }
+            if(r==l && r!=0 && l!=0){
+                count++;
+                r=0;l=0;
+            }
+        }
+        return count++;
+    }
+};
