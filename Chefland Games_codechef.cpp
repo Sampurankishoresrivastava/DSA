9251c292302fea20c3dee483b73ca40f35587d62
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2022-09-14 23:29:04 +0530

    Create Chefland Games_codechef.cpp

---
+++ Chefland Games_codechef.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    if(a==0 && b==0 && c==0 && d==0){
+	        cout<<"IN"<<endl;
+	    }
+	    else{
+	        cout<<"OUT"<<endl;
+	    }
+	}
+	return 0;
+}
