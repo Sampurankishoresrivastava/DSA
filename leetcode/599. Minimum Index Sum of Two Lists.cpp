2a2eb52cdde4ecec5eed2ed5bc6f464f857f2ce9
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2023-04-19 21:04:21 +0530

    Create 599. Minimum Index Sum of Two Lists.cpp

---
+++ leetcode/599. Minimum Index Sum of Two Lists.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        vector<string> v;
+        unordered_map<string,int> m;
+        int mini =INT_MAX;
+        int mi;
+        int sum;
+        for(int i=0;i<list1.size();i++){
+            m[list1[i]]=i;
+        }
+        for(int j=0;j<list2.size();j++){
+            if(m.find(list2[j])!=m.end()){
+                sum = j + m[list2[j]];
+                if(sum<mini){
+                    v.clear();
+                    mini = sum;
+                    v.push_back(list2[j]);
+                }
+                else if( sum == mini){
+                    v.push_back(list2[j]);
+                }
+            }
+        }
+        return v;
+    }
+};
