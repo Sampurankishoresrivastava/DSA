eef0b84dcec92d58c5cdc9658f056390ac0f402a
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2023-05-13 21:07:39 +0530

    Create 2466. Count Ways To Build Good Strings.cpp

---
+++ leetcode_problem-of-the-day/2466. Count Ways To Build Good Strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        int dp[high+1];
+    memset(dp, 0, sizeof(dp));
+    dp[0]=1;
+    int ans=0;
+    for(int i=1;i<=high;i++){
+        dp[i] = ((i-zero>=0 ? dp[i-zero]:0)+(i-one>=0 ? dp[i-one]:0))%1000000007;
+        if(i>=low){
+            ans = (ans+dp[i])%1000000007;
+        }
+    }
+    return ans;
+
+    }
+};
