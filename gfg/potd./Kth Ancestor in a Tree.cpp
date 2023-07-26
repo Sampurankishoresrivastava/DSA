9866ce79c9d7a77d812e1437d9d0dfad9cf42be4
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2023-07-26 21:02:45 +0530

    Create Kth Ancestor in a Tree.cpp

---
+++ gfg/potd./Kth Ancestor in a Tree.cpp
@@ -0,0 +1,34 @@
+Node* dfs(Node*root, int& k, int node){
+  if(root==NULL) return NULL;
+  if(root->data==node) return root;
+
+  Node* leftAns = dfs(root->left,k, node);
+  if(leftAns != NULL){
+    k--;
+    if(k<=0){
+      k = INT_MAX;
+      return root;
+    }
+    return leftAns;
+  }
+  
+  
+  Node* rightAns = dfs(root->right, k, node);
+  if(rightAns != NULL){
+    k--;
+    if(k<=0){
+      k=INT_MAX;
+      return root;
+    }
+    return rightAns;
+  }
+  return NULL;
+}
+int kthAncestor(Node *root, int k, int node)
+{
+    // Code here
+    Node* ans = dfs(root, k, node);
+  if(ans == NULL || ans->data == node)
+    return -1;
+  return ans->data;
+}
