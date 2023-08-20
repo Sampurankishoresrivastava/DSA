07b5d406911040d483f2200428584ce67efd96ff
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2023-08-20 22:46:42 +0530

    Create 2831. Find the Longest Equal Subarray.cpp

---
+++ leetcode/contest./2831. Find the Longest Equal Subarray.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestEqualSubarray(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        int n = nums.size();
+        int mxcount =0 ;
+        int back = 0 ;
+        int ans =0 ;
+        for(int  r =0;  r<n ; r++){
+            if(mp.find(nums[r]) == mp.end()){
+                mp[nums[r]] = 0;
+            }
+            
+            // adding into map
+            mp[nums[r]]++;
+            
+            
+            
+            // count max freq
+            mxcount = max(mxcount , mp[nums[r]]);
+            
+            if((r - back+1 - mxcount) > k){
+                mp[nums[back]] -= 1;
+                back++;
+            }
+            ans = max(ans , mxcount);
+        }
+        return ans;
+    }
+};
