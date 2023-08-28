db2f4c4601e33619c9123a159640b8faef61b6da
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2023-08-28 20:32:49 +0530

    Create 225. Implement Stack using Queues.cpp

---
+++ leetcode_problem-of-the-day/225. Implement Stack using Queues.cpp
@@ -0,0 +1,30 @@
+class MyStack {
+    queue<int> q1;
+    queue<int>q2;
+public:
+    MyStack() {
+    }
+    
+    void push(int x) {
+        q2.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1,q2);
+    }
+    
+    int pop() {
+        int result =top();
+        q1.pop();
+        return result;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
