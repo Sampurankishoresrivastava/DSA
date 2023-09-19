fd01c09bc1b89b87a2861beab73e7e662bb4d721
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2023-09-19 22:54:29 +0530

    Create 287. Find the Duplicate Number.cpp

---
+++ leetcode_problem-of-the-day/287. Find the Duplicate Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        // vector<int> num=nums;
+        // int ans;
+        // sort(nums.begin(),nums.end());
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         ans=nums[i];
+        //         break;
+        //     }
+        // }
+        // return ans;
+        unordered_map<int,int> count;
+        for(int it: nums){
+            count[it]++;
+        }
+        for(auto it: count){
+            if(it.second >=2){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
