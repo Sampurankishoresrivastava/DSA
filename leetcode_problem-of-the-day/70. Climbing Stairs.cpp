7daf5dc07f44ff1847ea8b802da8a3597f8d7e20
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2024-01-19 00:00:21 +0530

    Update 70. Climbing Stairs.cpp

--- leetcode_problem-of-the-day/70. Climbing Stairs.cpp
+++ leetcode_problem-of-the-day/70. Climbing Stairs.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        vector<int>dp(n+2,0);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        if(n<=2){
+            return dp[n];
         }
-        return climbStairs(n-1) + climbStairs(n-2);
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
