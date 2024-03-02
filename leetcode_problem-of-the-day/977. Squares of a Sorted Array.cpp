e5fc4853a438cd419f06cad9df8a4070546d84d5
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2024-03-02 22:11:49 +0530

    Update 977. Squares of a Sorted Array.cpp

--- leetcode_problem-of-the-day/977. Squares of a Sorted Array.cpp
+++ leetcode_problem-of-the-day/977. Squares of a Sorted Array.cpp
@@ -1,16 +1,41 @@
-class Solution{
-    public:
-    int firstElementKTime(int n, int k, int a[])
-    {
-        unordered_map<int,int> mp;
-        
-        for(int i=0;i<n;i++){
-            mp[a[i]]++;
-            if(mp[a[i]]==k){
-                return a[i];
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        // &&&&&&&&&&&&&This takes O(nlogn) tc&&&&&&&&&&&&&&&&&&&
+        // for(int i=0;i<nums.size();i++){
+        //     nums[i]=nums[i]*nums[i];
+        // }
+
+        // sort(nums.begin(),nums.end());
+
+        // return nums;
+
+
+        // **********This takes O(nlogn) tc**************
+        int n=nums.size();
+        int left=0;
+        int right=n-1;
+        vector<int> ans(n);
+
+        for(int i=n-1;i>=0;i--){
+            
+            if(abs(nums[left])>nums[right]){
+                ans[i]=nums[left]*nums[left];
+                left++;
+            }
+
+            else{
+                ans[i]=nums[right]*nums[right];
+                right--;
             }
         }
-        
-        return -1;
+
+        for(int i=0;i<n;i++){
+            nums[i]=ans[i];
+        }
+
+        return nums;
+
     }
 };
