64fc7612d003cabc039d82041833e4dbfb3ce7af
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2024-03-15 23:08:33 +0530

    Create Linked List that is Sorted Alternatingly.cpp

---
+++ gfg/potd./Linked List that is Sorted Alternatingly.cpp
@@ -0,0 +1,53 @@
+
+/*
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+*/
+
+class Solution
+{
+    public:
+    // your task is to complete this function
+    void sort(Node **head)
+    {
+         // Code here
+         Node *ptr=*head;
+         int count=0;
+         
+         while(ptr!=NULL){
+             count++;
+             ptr=ptr->next;
+         }
+         
+         vector<int> temp(count);
+         
+         Node *p=*head;
+         int i=0;
+         while(p!=NULL){
+            temp[i]=p->data;
+            p=p->next;
+            if (p!=NULL){
+                temp[i+1]=p->data;
+                p=p->next;
+            }
+            i+=2;
+        }
+         
+        std::sort(temp.begin(), temp.end());
+
+        Node *sol = *head;
+        for (int j=0;j<count;j++){
+            sol->data=temp[j];
+            sol=sol->next;
+        }
+         
+    }
+};
