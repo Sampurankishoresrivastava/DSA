b87a42049a7d2475c671e2fbffa55bde528dd53e
Sampuran Kishore Srivastava <90759917+Sampurankishoresrivastava@users.noreply.github.com>
2024-03-18 22:50:28 +0530

    Create Level order traversal.cpp

---
+++ gfg/potd./Level order traversal.cpp
@@ -0,0 +1,50 @@
+/* A binary tree Node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* root)
+    {
+      //Your code here
+      vector<int> ans;
+      
+      if(root==NULL){
+          return ans;
+      }
+      
+      queue<Node*> q;
+      q.push(root);
+      
+      while(!q.empty()){
+          Node* top=q.front();
+          ans.push_back(top->data);
+          q.pop();
+          
+          if(top->left!=NULL){
+            q.push(top->left);
+          }
+          
+          if(top->right!=NULL){
+            q.push(top->right);  
+          }
+      }
+      
+      return ans;
+      
+    }
+};
